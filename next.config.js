ed5d1604adb20f5076ca60747bde1cbf3431fa00
gissehel <public-devgit-common@gissehel.org>
2024-01-21 13:01:14 +0100

    deploy to /{reponame} if built from github actions

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
+    assetPrefix,
+    basePath,
 }
 
 module.exports = nextConfig
